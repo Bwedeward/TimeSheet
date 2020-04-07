2ce5c8c58ab9d18cf147759004c7ed1253d31daf
Tyler Blakeman <tylerblakeman@gmail.com>
2020-04-06 21:56:34 -0500

    created the on click function

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,31 @@
-//hello
+$("#add-info").on("click", function(event){
+    event.preventDefault();
+    var name = $("#name").val().trim()
+    var role = $("#role").val().trim()
+    var startDate = $("start-date").val().trim()
+    var monthlyRate = $("#monthly-rate").val().trim()
+    // <input type="text" id="name"><br>
+    // <input type="text" id="role"><br>
+    // <input type="text" id="start-date"><br>
+    // <input type="text" id="monthly-rate"><br>
+
+    var tabRow = "<tr>"
+    var throwingStuff;
+    var td = "<td>"
+    var tdMonthsWorked = "<td id>"
+    // throwingStuff = throwingStuff+tabRow;
+    // throwingStuff = throwingStuff+td+
+
+
+
+    // <tr>
+    // <th class="name-column">Name</th>
+    // <th class="role-column">Role</th>
+    // <th class="start-date-column">Start Date</th>
+    // <th class="months-worked-column">Months Worked</th>
+    // <th class="monthly-column">Monthly</th>
+    // </tr>
+
+
+})
+
